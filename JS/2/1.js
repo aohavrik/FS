29c576c88d4e107e14748beaa1d9e543a55e6a94
aohavrik <aohavrik@gmail.com>
2024-10-02 22:12:55 +0300

    les2 JS

---
+++ JS/2/1.js
@@ -0,0 +1,106 @@
+let a1 = 12// число
+let a2 = 'text123' //рядок
+let a3 = false // boole
+let a4 = null // пустота
+let a5 = undefined // ще більша пустота)
+let a6 = Symbol(123)//кожен символ який записано обератається в окрему обгортку і кожен не дорівнює іншому
+let a7 = BigInt(2400000) // для роботи з великими числами 
+let a8 = {}//обьект
+
+
+
+
+let b1 = 9+1
+document.write(b1);
+document.write('<hr>');
+
+b1 += 10
+document.write(b1);
+document.write('<hr>');
+
+let b2 = 10-1
+document.write(b2);
+document.write('<hr>');
+
+let b3 = b1+1
+document.write(b1);
+document.write('<hr>');
+
+let b4 = b1/2
+document.write(b1);
+document.write('<hr>');
+
+let b5 = b4*3
+document.write(b1);
+document.write('<hr>');
+
+let b6 = (b3*2)/(b2*3)
+document.write(b1);
+document.write('<hr>');
+
+let b7 = b1*3
+document.write(b1);
+document.write('<hr>');
+
+let b8 = 13-2
+document.write(b1);
+document.write('<hr>');
+
+let b9 = ++b1
+document.write(b1);
+document.write('<hr>');
+
+let b10 = b3+b8
+document.write(b1);
+document.write('<hr>');
+
+
+
+
+
+let с1 = "123"
+let num = Number(с1) 
+console.log(с1)
+
+let с2 = 0
+let с22 = Boolean(с2)
+console.log(с22)
+
+let с3 = 123;
+let с32 = Boolean(с3); 
+console.log(с32); 
+
+let с4 = 456
+let с42 = String(с4)
+console.log(с42)
+
+let  с5 = false
+let с52 = String(с5)
+console.log(с52)
+
+let c6 = "h2o"
+let c62 = Number(c6)
+console.log(c62)
+
+
+
+
+
+
+let d1 = 1+1
+console.log(d1)
+
+let d2 = 2*2
+console.log(d1)
+
+let d3 = 10/2
+console.log(d1)
+
+let d4 = 10-3
+console.log(d1)
+
+let d5 = 10%3
+console.log(d1)
+
+let d6 = 6**2
+console.log(d1)
