a9f0dda7f7ec4cc3f177f537bf204759975f92d2
Andrii Havrik <aohavrik@gmail.com>
2024-11-27 20:17:48 +0200

    PY

--- python/рядки, списки ч2/2.py
+++ python/рядки, списки ч2/2.py
@@ -4,7 +4,6 @@
 # ■ елементи обох списків;
 def sumArr(arr1, arr2):
     arr = arr1 + arr2
-    print(f'елементи обох списків: {arr}')
     return (arr)
 # ■ елементи обох списків без повторень;
 def noreapet(arr1, arr2):
@@ -15,7 +14,6 @@ def noreapet(arr1, arr2):
     for j in arr2:
         if j not in arr:
             arr.append(j)
-    print(f'елементи обох списків без повторень {arr}')
     return (arr)
 # ■ елементи, спільні для двох списків;
 def repetArr(arr1, arr2):
@@ -23,23 +21,21 @@ def repetArr(arr1, arr2):
     for i in arr1:
         if i in arr2:
             arr.append(i)
-    print("елементи, спільні для двох списків:", arr)
+    return (arr)
 # ■ тільки унікальні елементи кожного зі списків;
 def universal(stock):
     arr = []
     for i in stock:
         if i not in random_nums2 and i not in arr:
             arr.append(i)
-
-    print(f'унікальні елементи кожного зі списків: {arr}')
+    return (arr)
 # ■ тільки мінімальне та максимальне значення кожного зі
 # списків.
 def minmaxArr(minmax):
     arr = []
     arr.append(min(minmax))
     arr.append(max(minmax))
-    print(f'minmax {arr}')
-
+    return (arr)
 import random
 random_nums1 = []
 random_nums2 = []
@@ -52,11 +48,19 @@ for _ in range(10):
     random_nums2.append(num2)
 print(f'масив 1: {random_nums1} \nмасив 2: {random_nums2}')
 
-sumArr(random_nums1, random_nums2)
-noreapet(random_nums1, random_nums2)
-repetArr(random_nums1, random_nums2)
-universal(noreapet(random_nums1, random_nums2))
-minmaxArr(sumArr(random_nums1, random_nums2))
+
+print(f"""
+елементи обох списків:
+{sumArr(random_nums1, random_nums2)}
+елементи обох списків без повторень:
+{noreapet(random_nums1, random_nums2)}
+елементи, спільні для двох списків:
+{repetArr(random_nums1, random_nums2)}
+тільки унікальні елементи кожного зі списків:
+{universal(noreapet(random_nums1, random_nums2))}
+тільки унікальні елементи кожного зі списків:
+{minmaxArr(sumArr(random_nums1, random_nums2))}
+""")
 
 
 
