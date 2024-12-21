0d124a76ce27d38336c6d16989c4bb0e856e1626
Andrii Havrik <aohavrik@gmail.com>
2024-12-21 13:46:42 +0200

    готовое 25 задание

--- JS/25 BOM Form API/1.js
+++ JS/25 BOM Form API/1.js
@@ -1,18 +1,32 @@
 // * Розділяти всі завдання коментарями і писати що треба робити в завданні.
 
 // 1. Створити промпт при заповненні якого вас перенаправляє на сайт який був введений в промпті.
-const prom = prompt('Введіть')
+// const prom = prompt('Введіть')
 
-if (prom.startsWith("http")) {
-    window.location.href = prom
-    console.log('aaa');
-    
-  } else {
-    window.location.href = "http://"+prom
-    console.log('aaassss');
-    
-  }
+// if (prom.startsWith("http")) {
+//     window.location.href = prom
+//     console.log('aaa');
+
+//   } else {
+//     window.location.href = "http://"+prom
+//     console.log('aaassss');
+
+//   }
 
 // 2. Створити функцію яка буде показувати на екрані геолокацію та url шлях сайту.
+function info() {
+  document.write(window.location.href);
+  window.navigator.geolocation.getCurrentPosition((geo) => {
+    console.log(geo);
+  })
 
+}
+// info()
 // 3. Створити стрім із мікро і відео (Див. лекцію).
+const vid = document.getElementById('vid')
+window.navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(vidos => {
+  vid.srcObject = vidos
+  vid.play()
+})
+
+// stream()
