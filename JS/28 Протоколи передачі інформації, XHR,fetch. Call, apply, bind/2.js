8c78db70dc703b11e9b3f67cd181a5366c18a872
AOHavrik <aohavrik@gmail.com>
2025-01-30 10:41:35 +0200

    28

---
+++ JS/28 Протоколи передачі інформації, XHR,fetch. Call, apply, bind/2.js
@@ -0,0 +1,18 @@
+// * Разделять все задания комментариями и писать что нужно делать в задании.
+
+// 1. Сделать страницу с произвольным контентом и переходами на 3 страницы:
+//    users
+//    posts
+//    comments
+
+// 2. На странице users вывести в html список пользователей через fetch и XMLHttpRequest.
+//    Стилизовать подходящим образом.
+//    https://jsonplaceholder.typicode.com/users
+
+// 3. На странице posts вывести в html список постов через fetch и XMLHttpRequest.
+//    Стилизовать подходящим образом.
+//    https://jsonplaceholder.typicode.com/posts
+
+// 4. На странице comments вывести в HTML список комментариев через fetch и XMLHttpRequest.
+//    Стилизовать подходящим образом.
+//    https://jsonplaceholder.typicode.com/comments
