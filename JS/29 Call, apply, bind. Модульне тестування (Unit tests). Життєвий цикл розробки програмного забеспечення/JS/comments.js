78d747496f14ee0b43071b1412a36c7edf4cacec
AOHavrik <aohavrik@gmail.com>
2025-02-04 20:30:05 +0200

    доне

--- JS/29 Call, apply, bind. Модульне тестування (Unit tests). Життєвий цикл розробки програмного забеспечення/JS/comments.js
+++ JS/29 Call, apply, bind. Модульне тестування (Unit tests). Життєвий цикл розробки програмного забеспечення/JS/comments.js
@@ -7,51 +7,57 @@
 
 
 
-// // 4. На странице comments вывести в HTML список комментариев через fetch и XMLHttpRequest.
-// //    Стилизовать подходящим образом.
-// //    https://jsonplaceholder.typicode.com/comments
+const comsDOM = document.getElementById("coms")
+const comsDOM2 = document.getElementById("coms2")
+
+const comsInput = document.getElementById("comsInput")
+const comsInput2 = document.getElementById("comsInput2")
+
+const comsButton = document.getElementById("comsButton")
+const comsButton2 = document.getElementById("comsButton2")
+
+comsButton.addEventListener("click", () => {
+    
+    const comsId = +comsInput.value
+
+    fetch('https://jsonplaceholder.typicode.com/comments').then(res => {
+    
+        console.log(res);
+        return res.json()
+    
+    }).then(comes => {
+        const filter = comes.filter(post => post.id == comsId);
+        console.log(filter);
+        filter.forEach((coms) => {
+            const newComs = document.createElement("li")
+            newComs.classList.add('com')
+            newComs.innerHTML = `${coms.id}.${coms.email}  <br><br><b>${coms.name}</b> <br><br>${coms.body}`
+            comsDOM.appendChild(newComs)
+        })
+    })
+
+})
 
 
-// const comDOM = document.getElementById('com')
 
-// fetch('https://jsonplaceholder.typicode.com/comments')
-//     .then(res => res.json())
-//     .then(com_then => {
-//         let i = 0
-//         com_then.slice(0, 10).forEach((coms) => {
-//             const newComs = document.createElement('div')
-//             newComs.classList.add('comments')
-//             newComs.innerHTML = `${coms.id}.${coms.email}  <br><br><b>${coms.name}</b> <br><br>${coms.body}`
-//             comDOM.appendChild(newComs)
+comsButton2.addEventListener("click", () => {
+    
+    const comsId2 = +comsInput2.value
+    
+    const getComs = new XMLHttpRequest()
+    getComs.open("GET", "https://jsonplaceholder.typicode.com/comments")
+    getComs.send();
+    getComs.onload = ({ target: { response } }) => {
 
-//             if (i < 10) {
-//                 i += 1
-//             } else {
-//                 return false
-//             }
-//         });
+        const coms2 = JSON.parse(response)
+        const filter = coms2.filter(coms => coms.id == comsId2)
 
-//     })
+        filter.forEach((coms) => {
+            const newComs2 = document.createElement("li")
+            newComs2.classList.add("coms")
+            newComs2.innerHTML = `${coms.id}.${coms.email}  <br><br><b>${coms.name}</b> <br><br>${coms.body}`
+            comsDOM2.appendChild(newComs2)
+        })
+    }
 
-// const comDOM2 = document.getElementById('com2')
-// const getComs = new XMLHttpRequest()
-
-// getComs.open('GET', 'https://jsonplaceholder.typicode.com/comments')
-// getComs.send()
-// getComs.onload = ({target:{response} }) => {
-//     let i = 0
-//     const coms2 = JSON.parse(response)
-//     coms2.slice(0, 10).forEach((coms) => {
-//         const newComs2 = document.createElement('div')
-//         newComs2.classList.add('comments')
-//         newComs2.innerHTML = `${coms.id}.${coms.email}  <br><br><b>${coms.name}</b> <br><br>${coms.body}`
-//         comDOM2.appendChild(newComs2)
-
-//         if (i < 10) {
-//             i += 1
-//         } else {
-//             return false
-//         }
-//     })
-
-// }
+})
--- JS/29 Call, apply, bind. Модульне тестування (Unit tests). Життєвий цикл розробки програмного забеспечення/JS/posts.js
+++ JS/29 Call, apply, bind. Модульне тестування (Unit tests). Життєвий цикл розробки програмного забеспечення/JS/posts.js
@@ -1,53 +1,63 @@
-// 3. На странице posts вывести в html пост через input[type=number] получать id пост и по клику на кнопку
-//    Send вывести пост в HTML через fetch и XMLHttpRequest.
-//    Стилизовать подходящим образом.
-//    https://jsonplaceholder.typicode.com/posts
+    // 3. На странице posts вывести в html пост через input[type=number] получать id пост и по клику на кнопку
+    //    Send вывести пост в HTML через fetch и XMLHttpRequest.
+    //    Стилизовать подходящим образом.
+    //    https://jsonplaceholder.typicode.com/posts
 
 
 
-// // 3. На странице posts вывести в html список постов через fetch и XMLHttpRequest.
-// //    Стилизовать подходящим образом.
-// //    https://jsonplaceholder.typicode.com/posts
-// const postDOM = document.getElementById('posts')
+const postsDOM = document.getElementById("posts")
+const postsDOM2 = document.getElementById("posts2")
 
-// fetch('https://jsonplaceholder.typicode.com/posts')
-//     .then(res => res.json())
-//     .then(post_then => {
-//         let i = 0
-//         post_then.slice(0, 10).forEach((pos) => {
-//             const newPost = document.createElement('div')
-//             newPost.classList.add('post')
-//             newPost.innerHTML = `${pos.id}. <b>${pos.title}</b> <br>${pos.body}`
-//             postDOM.appendChild(newPost)
+const postsInput = document.getElementById("postsInput")
+const postsInput2 = document.getElementById("postsInput2")
 
-//             if (i < 10) {
-//                 i += 1
-//             } else {
-//                 return false
-//             }
-//         });
+const postsButton = document.getElementById("postsButton")
+const postsButton2 = document.getElementById("postsButton2")
 
-//     })
+postsButton.addEventListener("click", () => {
+    
+    const postId = +postsInput.value
 
-// const postDOM2 = document.getElementById('posts2')
-// const getPost = new XMLHttpRequest()
+    fetch('https://jsonplaceholder.typicode.com/posts').then(res => {
+    
+        console.log(res);
+        return res.json()
+    
+    }).then(posts => {
+        const filter = posts.filter(post => post.id == postId);
+        console.log(filter);
+        filter.forEach((post) => {
+            const newPost = document.createElement("li")
+            newPost.classList.add('post')
+            newPost.innerHTML = `${post.id}. <b>${post.title}</b> <br>${post.body}`
+            postsDOM.appendChild(newPost)
+        })
+    })
 
-// getPost.open('GET', 'https://jsonplaceholder.typicode.com/posts')
-// getPost.send()
-// getPost.onload = ({target:{response} }) => {
-//     let i = 0
-//     const posts2 = JSON.parse(response)
-//     posts2.slice(0, 10).forEach((pos) => {
-//         const newPost2 = document.createElement('div')
-//         newPost2.classList.add('post')
-//         newPost2.innerHTML = `${pos.id}. <b>${pos.title}</b> <br>${pos.body}`
-//         postDOM2.appendChild(newPost2)
+})
 
-//         if (i < 10) {
-//             i += 1
-//         } else {
-//             return false
-//         }
-//     })
 
-// }
+
+postsButton2.addEventListener("click", () => {
+    
+    const postId2 = +postsInput2.value
+    
+    const getPost = new XMLHttpRequest()
+    getPost.open("GET", "https://jsonplaceholder.typicode.com/posts")
+    getPost.send();
+    getPost.onload = ({ target: { response } }) => {
+
+        const posts2 = JSON.parse(response)
+        console.log(response);
+
+        const filter = posts2.filter(post => post.id == postId2)
+
+        filter.forEach((post) => {
+            const newPost2 = document.createElement("li")
+            newPost2.classList.add("post")
+            newPost2.innerHTML = `${post.id}. <b>${post.title}</b> <br>${post.body}`
+            postsDOM2.appendChild(newPost2)
+        })
+    }
+
+})
