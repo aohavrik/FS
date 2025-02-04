d33560d6651ddc28fd7d01967e91ed859526d86d
AOHavrik <aohavrik@gmail.com>
2025-02-04 19:03:14 +0200

    сделал юзерс

--- JS/29 Call, apply, bind. Модульне тестування (Unit tests). Життєвий цикл розробки програмного забеспечення/JS/users.js
+++ JS/29 Call, apply, bind. Модульне тестування (Unit tests). Життєвий цикл розробки програмного забеспечення/JS/users.js
@@ -1,5 +1,3 @@
-
-
 // 2. На странице users через input[type=number] получать id юзерата и по щелчку на кнопку
 //    Send вывести пользователя в HTML через fetch и XMLHttpRequest.
 //    Стилизовать подходящим образом.
@@ -14,20 +12,41 @@ const userInput2 = document.getElementById("userInput2")
 const userButton = document.getElementById("userButton")
 const userButton2 = document.getElementById("userButton2")
 
-const getUser = new XMLHttpRequest()
+userButton.addEventListener("click", () => {
+    
+    const userId = +userInput.value
+    fetch('https://jsonplaceholder.typicode.com/users').then(res => {
+    
+        return res.json()
+    
+    }).then(us => {
+        const filter = us.filter(us => us.id == userId);
+        console.log(filter);
+        filter.forEach((user) => {
+            const newUser = document.createElement("li")
+            newUser.classList.add('user')
+            newUser.innerHTML = `${user.id}. ${user.name} <br>${user.email}`
+            usersDOM.appendChild(newUser)
+        })
+    })
+
+})
+
+
 
 userButton2.addEventListener("click", () => {
     
     const userId2 = +userInput2.value
-
+    
+    const getUser = new XMLHttpRequest()
     getUser.open("GET", "https://jsonplaceholder.typicode.com/users")
     getUser.send();
     getUser.onload = ({ target: { response } }) => {
 
         const users2 = JSON.parse(response)
-        console.log(users2);
+        console.log(response);
 
-        const filter = users2.filter((user) => user.id == userId2)
+        const filter = users2.filter(us => us.id == userId2)
 
         filter.forEach((user) => {
             const newUser2 = document.createElement("li")
@@ -40,20 +59,4 @@ userButton2.addEventListener("click", () => {
 })
 
 
-//ниже готовое:
 
-// const userId = +userInput.value
-// fetch('https://jsonplaceholder.typicode.com/users').then(res => {
-
-//     return res.json()
-
-// }).then(us => {
-//     const filter = us.filter(us => us.id == userId);
-//     console.log(filter);
-//     filter.forEach((user) => {
-//         const newUser = document.createElement("li")
-//         newUser.classList.add('user')
-//         newUser.innerHTML = `${user.id}. ${user.name} <br>${user.email}`
-//         usersDOM.appendChild(newUser)
-//     })
-// })
