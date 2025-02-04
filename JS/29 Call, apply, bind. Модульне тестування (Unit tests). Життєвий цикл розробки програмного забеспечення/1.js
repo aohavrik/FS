0a0d3bdf1f8ef47a4f92089ac066d5cfa6befb27
AOHavrik <aohavrik@gmail.com>
2025-02-04 20:53:09 +0200

    123

--- JS/29 Call, apply, bind. Модульне тестування (Unit tests). Життєвий цикл розробки програмного забеспечення/1.js
+++ JS/29 Call, apply, bind. Модульне тестування (Unit tests). Життєвий цикл розробки програмного забеспечення/1.js
@@ -12,10 +12,16 @@ let Petro = {
     age: 30
 }
 
-
+let Ivan = {
+    name: 'іван',
+    age: 55
+}
 
 names.sayHallo.bind(Petro)(22)
+names.sayHallo.bind(Ivan)(22)
 
 names.sayHallo.call(Petro, 322)
+names.sayHallo.call(Ivan, 322)
 
 names.sayHallo.apply(Petro, [222])
+names.sayHallo.apply(Ivan, [222])
