30d258acf110dfc33974563a10bb481fb762f9e2
AOHavrik <aohavrik@gmail.com>
2025-03-04 09:59:29 +0200

    тайпскрипт

--- React/05_TypeScript/src/ls.js
+++ React/05_TypeScript/src/ls.js
@@ -1,1 +1,39 @@
-console.log(22);
+var usDom = document.getElementById('users');
+if (usDom) {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(function (resp) { return resp.json(); })
+        .then(function (users) {
+        users.forEach(function (user) {
+            var newUserDiv = document.createElement('div');
+            newUserDiv.classList.add('users-style');
+            newUserDiv.innerHTML = "".concat(user.id, ". ").concat(user.name, " - ").concat(user.email, " (").concat(user.website, ")");
+            usDom.appendChild(newUserDiv);
+        });
+    });
+}
+var postDOM = document.getElementById('posts-style');
+if (postDOM) {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(function (resp) { return resp.json(); })
+        .then(function (posts) {
+        posts.forEach(function (post) {
+            var newPostDiv = document.createElement('div');
+            newPostDiv.classList.add('posts-style');
+            newPostDiv.innerHTML = "<h4 class=\"braun\">".concat(post.id, ". ").concat(post.title, "</h4><hr><p>").concat(post.body, "</p>");
+            postDOM.appendChild(newPostDiv);
+        });
+    });
+}
+var commDom = document.getElementById('comments-style');
+if (commDom) {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+        .then(function (resp) { return resp.json(); })
+        .then(function (comments) {
+        comments.forEach(function (comment) {
+            var newCommentDiv = document.createElement('div');
+            newCommentDiv.classList.add('comments-style');
+            newCommentDiv.innerHTML = "<h4 class=\"braun\">".concat(comment.name, ".</h4> <div>").concat(comment.email, "</div><p>").concat(comment.body, "</p>");
+            commDom.appendChild(newCommentDiv);
+        });
+    });
+}
--- React/05_TypeScript/src/ls.ts
+++ React/05_TypeScript/src/ls.ts
@@ -1,1 +1,69 @@
-console.log(22);
+
+
+interface User {
+    id: number;
+    name: string;
+    email: string;
+    website: string;
+}
+
+const usDom: HTMLElement | null = document.getElementById('users');
+
+if (usDom) {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(resp => resp.json())
+        .then((users: User[]) => {
+            users.forEach((user) => {
+                const newUserDiv = document.createElement('div');
+                newUserDiv.classList.add('users-style');
+                newUserDiv.innerHTML = `${user.id}. ${user.name} - ${user.email} (${user.website})`;
+                usDom.appendChild(newUserDiv);
+            });
+        });
+}
+
+
+
+
+interface Post {
+    id: number;
+    title: string;
+    body: string;
+}
+
+const postDOM: HTMLElement | null = document.getElementById('posts-style');
+
+if (postDOM) {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(resp => resp.json())
+        .then((posts: Post[]) => {
+            posts.forEach((post) => {
+                const newPostDiv = document.createElement('div');
+                newPostDiv.classList.add('posts-style');
+                newPostDiv.innerHTML = `<h4 class="braun">${post.id}. ${post.title}</h4><hr><p>${post.body}</p>`;
+                postDOM.appendChild(newPostDiv);
+            });
+        });
+}
+
+
+interface Comment {
+    name: string;
+    email: string;
+    body: string;
+}
+
+const commDom: HTMLElement | null = document.getElementById('comments-style');
+
+if (commDom) {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+        .then(resp => resp.json())
+        .then((comments: Comment[]) => {
+            comments.forEach((comment) => {
+                const newCommentDiv = document.createElement('div');
+                newCommentDiv.classList.add('comments-style');
+                newCommentDiv.innerHTML = `<h4 class="braun">${comment.name}.</h4> <div>${comment.email}</div><p>${comment.body}</p>`;
+                commDom.appendChild(newCommentDiv);
+            });
+        });
+}
